c13e538cfabe894a11f1d9091a877ffa33cf10d0
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-02-24 18:49:25 -0500

    home page improvement

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-console.log("testing");
+// window.onscroll = function() {scrollFunction()};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+//     document.getElementById("top-navbar").style.top = "0";
+//   } else {
+//     document.getElementById("top-navbar").style.top = "-50px";
+//   }
+// }
