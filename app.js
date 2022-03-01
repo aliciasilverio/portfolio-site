25b9fa4018c5672d6c00780c11a95ba93ee8a105
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-02-28 21:16:23 -0500

    working on bio

--- app.js
+++
@@ -1,9 +0,0 @@
-// window.onscroll = function() {scrollFunction()};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-//     document.getElementById("top-navbar").style.top = "0";
-//   } else {
-//     document.getElementById("top-navbar").style.top = "-50px";
-//   }
-// }
