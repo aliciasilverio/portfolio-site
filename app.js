8f75656e04c7949219d6205915716bd2d0092480
Alicia Silverio <aliciaalicia2455@gmail.com>
2022-03-02 18:49:36 -0500

    better style for slideshow gallery

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // 1. When the user scrolls down, hide the navbar. When the user scrolls up, show the navbar https://www.w3schools.com/howto/howto_js_navbar_hide_scroll.asp
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
+  var currentScrollPos = window.pageYOffset; 
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("navbar").style.top = "0";
   } else {
